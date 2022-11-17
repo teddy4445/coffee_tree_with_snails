67864675211a10e8f56f8fe7313f60994fc04d07
Teddy Lazebnik <lazebnik.teddy@gmail.com>
2022-11-17 11:06:07 +0200

    working model run and templates for the analysis

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # library imports
 import os
-import glob
+import time
 import oct2py
-import numpy as np
 import pandas as pd
-import scipy.io as sio
 
 # project imports
 
 # initialize for the system
 from plotter import Plotter
+from mat_file_loader import MatFileLoader
+
 
 oct2py.octave.addpath(os.path.dirname(__file__))
 
@@ -23,6 +23,7 @@ class Main:
     OCTAVE_BASED_SCRIPT_NAME = "model_solver.txt"
     OCTAVE_RUN_SCRIPT_NAME = "model_solver.m"
     OCTAVE_RUN_RESULT_NAME = "model_answer.mat"
+
     # END - CONSTS #
 
     def __init__(self):
@@ -36,17 +37,14 @@ class Main:
         """
         # make sure the IO is file
         Main.io()
-        # baseline graph - just run the model and plot it
-        Plotter.baseline(model_matrix=Main.solve_the_model(),
-                         save_path="baseline.pdf")
-        # sensitivity graph (1) - pick param 1
-        # TODO: good luck
-        # sensitivity graph (2) - pick param 2
-        # TODO: good luck
-        # sensitivity graph (3) - pick param 3
-        # TODO: good luck
-        # sensitivity graph (4) - pick param 4
-        # TODO: good luck
+        # baseline graphs
+        Main.first_plot()
+        # one-dim sensitivity graphs
+        Main.second_graph()
+        # heatmap sensitivity graphs
+        Main.third_graph()
+        # optimal sub-section graphs
+        Main.fourth_graph()
 
     @staticmethod
     def io() -> None:
@@ -56,6 +54,70 @@ class Main:
                 os.mkdir(os.path.join(os.path.dirname(__file__), name))
             except:
                 pass
+
+    @staticmethod
+    def desire_metric(df: dict) -> tuple:
+        # TODO: add later, return mean & std
+        pass
+
+    @staticmethod
+    def first_plot() -> None:
+        # baseline graph - just run the model and plot it
+        initial_conditions = [
+            [100, 5, 100],
+            [0, 100, 5],
+            [97, 3, 50],
+            [25, 25, 25],
+        ]
+
+        for index, initial_condition in enumerate(initial_conditions):
+            Plotter.baseline(model_matrix=Main.solve_the_model(initial_condition=initial_condition),
+                             save_path="baseline_{}.pdf".format(index))
+
+    @staticmethod
+    def second_graph() -> None:
+        ans_mean = []
+        ans_std = []
+        x = [0.05 * i for i in range(8)]
+        for parm_val in x:
+            mean, std = Main.desire_metric(df=Main.solve_the_model(a=parm_val))
+            ans_mean.append(mean)
+            ans_std.append(std)
+        Plotter.sensitivity(x=x,
+                            y=ans_mean,
+                            y_err=ans_std,
+                            x_label="a",
+                            y_label="add-later",
+                            save_path="sensitivity_{}.pdf".format("a"))
+
+        # TODO: repeat the above code for beta, k, gamma, b, d
+
+    @staticmethod
+    def third_graph() -> None:
+        x = [0.05 * i for i in range(8)]  # TODO: replace with better range
+        y = [0.05 * i for i in range(8)]  # TODO: replace with better range
+        answer = []
+        for x_parm_val in x:
+            row = []
+            for y_parm_val in y:
+                mean, std = Main.desire_metric(df=Main.solve_the_model(a=x_parm_val,
+                                                                       gamma=y_parm_val))
+                row.append(mean)
+            answer.append(row)
+        df = pd.DataFrame(data=answer,
+                          columns=x,
+                          index=y)
+        Plotter.heatmap(df=df,
+                        x_label="a",
+                        y_label="$\gamma$",
+                        save_path="heatmap_{}_{}.pdf".format("a", "gamma"))
+
+        # TODO: repeat the above code for beta cross k and b cross d
+
+    @staticmethod
+    def fourth_graph() -> None:
+        # TODO: think about it later
+        pass
 
     @staticmethod
     def solve_the_model(tspan: list = None,
@@ -81,7 +143,7 @@ class Main:
             raise Exception("Main.solve_the_model: all parameter values should be non-negative.")
 
         # load generic script
-        with open(os.path.join(os.path.dirname(__file__), Main.OCTAVE_BASED_SCRIPT_NAME)) as m_source_file:
+        with open(os.path.join(os.path.dirname(__file__), Main.OCTAVE_BASED_SCRIPT_NAME), "r") as m_source_file:
             script = m_source_file.read()
         # update the code
         script = script.format(tspan,
@@ -93,12 +155,15 @@ class Main:
                                b,
                                d)
         # save the file for run
-        with open(os.path.join(os.path.dirname(__file__), Main.OCTAVE_RUN_SCRIPT_NAME)) as m_run_file:
+        with open(os.path.join(os.path.dirname(__file__), Main.OCTAVE_RUN_SCRIPT_NAME), "w") as m_run_file:
             m_run_file.write(script)
         # run the script
         oct2py.octave.run(os.path.join(os.path.dirname(__file__), Main.OCTAVE_RUN_SCRIPT_NAME))
+        # wait to make sure the file is written
+        time.sleep(3)
         # load the result file
-        return sio.loadmat(os.path.join(os.path.dirname(__file__), Main.OCTAVE_RUN_RESULT_NAME))
+        return MatFileLoader.read(path=os.path.join(os.path.dirname(__file__), Main.OCTAVE_RUN_RESULT_NAME),
+                                  delete_in_end=True)
 
 
 if __name__ == '__main__':
--- plotter.py
+++ plotter.py
@@ -1,5 +1,8 @@
 # library imports
 import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # project imports
@@ -18,17 +21,35 @@ class Plotter:
         pass
 
     @staticmethod
-    def baseline(model_matrix,
+    def baseline(model_matrix: dict,
                  save_path: str = "baseline.pdf"):
         # TODO: plot the run itself
         ax = plt.subplot(111)
 
-        # plt.plot()
-        # plt.plot()
-        # plt.plot()
+        plt.plot(model_matrix["t"],
+                 np.asarray(model_matrix["y"])[:, 0],
+                 "-",
+                 color="green",
+                 label="$T_s(t)$")
+
+        plt.plot(model_matrix["t"],
+                 np.asarray(model_matrix["y"])[:, 1],
+                 "--",
+                 color="red",
+                 label="$T_i(t)$")
+
+        plt.plot(model_matrix["t"],
+                 np.asarray(model_matrix["y"])[:, 2],
+                 "-.",
+                 color="blue",
+                 label="$S(t)$")
         plt.xlabel("Time", fontsize=14)
         plt.ylabel("Population size", fontsize=14)
+        plt.xlim((min(model_matrix["t"]), max(model_matrix["t"])))
+        plt.ylim((0, np.max(np.asarray(model_matrix["y"]))))
         plt.legend()
+        plt.grid(alpha=0.25,
+                 color="black")
         ax.yaxis.set_ticks_position('left')
         ax.xaxis.set_ticks_position('bottom')
         plt.savefig(save_path, dpi=600)
@@ -41,7 +62,6 @@ class Plotter:
                     x_label: str,
                     y_label: str,
                     save_path: str):
-        # TODO: plot the run itself
         ax = plt.subplot(111)
         plt.errorbar(x,
                      y,
@@ -54,6 +74,22 @@ class Plotter:
         plt.ylabel(y_label, fontsize=14)
         ax.yaxis.set_ticks_position('left')
         ax.xaxis.set_ticks_position('bottom')
+        plt.savefig(save_path, dpi=600)
+        plt.close()
+
+    @staticmethod
+    def heatmap(df: pd.DataFrame,
+                x_label: str,
+                y_label: str,
+                save_path: str):
+        # TODO: after picking the metric, rethink about the limitions - i.e., the vmin & vmax arguments
+        sns.heatmap(df,
+                    vmin=0,
+                    vmax=1,
+                    annot=False,
+                    cmap="coolwarm")
+        plt.xlabel(x_label, fontsize=14)
+        plt.ylabel(y_label, fontsize=14)
         plt.savefig(save_path, dpi=600)
         plt.close()
 
